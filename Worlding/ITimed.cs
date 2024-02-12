766d5d0f72051c9ccd92b7b54683129029010143
Ivan Saez Zamora <ivsaez@gmail.com>
2024-02-12 22:20:06 +0100

    update package

--- Worlding/ITimed.cs
+++ Worlding/ITimed.cs
@@ -1,16 +1,9 @@
-﻿using Agents;
-using Items;
-using Mapping;
-using Outputer;
-using Saver;
+﻿using Outputer;
 
 namespace Worlding
 {
     public interface ITimed
     {
-        Output OnTurnPassed<A, I, M>(IWorld<A, I, M> world, int turns)
-            where A : IAgent, ITimed, ISavable, ICloneable
-            where I : IItem, ITimed, ISavable, ICloneable
-            where M : IMapped, ITimed, ISavable, ICloneable;
+        Output OnTurnPassed(IWorld world, int turns);
     }
 }
